c63adbb51171cfb7a1731d1270c16dbabb776c75
Dan Hayden <dan@tutonics.com>
2017-07-13 10:21:48 +0100

    chore: update dependencies and build (small improvement to bundle size)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
-import {minify} from 'uglify-js-harmony'
+import {minify} from 'uglify-es'
 import fs from 'fs'
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
@@ -13,9 +13,5 @@ export default {
     {dest: pkg.module, format: 'es'},
     {dest: pkg['umd:main'], format: 'umd'}
   ],
-  plugins: [
-    resolve(),
-    babel({exclude: 'node_modules/**'}),
-    uglify({}, minify)
-  ]
+  plugins: [resolve(), babel({exclude: 'node_modules/**'}), uglify({}, minify)]
 }
